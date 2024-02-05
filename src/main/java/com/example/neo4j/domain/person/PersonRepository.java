a1ace89aa35dc2a20b8d12e62595778fc7e5ff3c
just chickenwing <a-wuethrich@hotmail.com>
2024-02-05 09:23:46 +0100

    removed person

--- src/main/java/com/example/neo4j/domain/person/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.neo4j.domain.person;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends Neo4jRepository<Person, Long> {
-}
